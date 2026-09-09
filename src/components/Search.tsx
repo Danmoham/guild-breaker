eb0599bdb2fdc66334dd6e81a82b328efd67633e
Daniel Mohammadian <dmoham35@gmail.com>
2026-09-09 12:48:51 +0100

    refactor(Search): remove placeholder prop and use default

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,13 +7,11 @@ import type {
 interface SearchProps {
   filtersState: PlayerFiltersState;
   dispatchFiltersAction: Dispatch<PlayerFiltersAction>;
-  placeholder?: string;
 }
 
 function Search({
   filtersState,
   dispatchFiltersAction,
-  placeholder = 'Search players...',
 }: SearchProps) {
   return (
     <div className="search">
@@ -21,7 +19,7 @@ function Search({
         type="text"
         className="search-input"
         value={filtersState.searchQuery}
-        placeholder={placeholder}
+        placeholder={'Search players...'}
         onChange={(event) =>
           dispatchFiltersAction({ type: 'SET_SEARCH_QUERY', searchQuery: event.target.value })
         }
